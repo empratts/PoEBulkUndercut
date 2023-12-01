c2f5b4e39730729402d44554a6ac4c43904b61e7
Eric Pratts <eric.pratts.1@us.af.mil>
2023-12-01 07:50:00 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# PoE Bulk Undercut
+
+PoE Bulk Undercut is a browser extension that interacts with the official Path of Exile bulk trade website. It provides a button on each search result that reads the contents of the clipboard and, if it contains the description of the item (obtained from hovering the item in game and pressing Ctrl-C) for sale in that result, replaces it with the text needed to price the stack of items such that it will appear above the listing on the trade site.
+
+## Workflow
+
+- Use a trade tool to open a search for the item with a minimum stock set (or do it manually)
+    - Ensure that the tool leaves the item description on the clipboard
+- Choose the listing that you want to undercut
+- Right Click on the item in-game (assuming it is in a public tab)
+- Paste
+- Press Enter
---
+++ button.js
@@ -0,0 +1,64 @@
+let clipText = "";
+let target = document.getElementById("app");
+
+let windowObserver = new MutationObserver(windowListener);
+
+function windowListener(mutationList) {
+    for (mutation of mutationList) {
+        if (mutation.target.className == 'results') {
+            let rows = mutation.target.getElementsByClassName('row exchange');
+            for (row of rows) {
+                var hasButton = row.querySelector('.btns > .btn-group > button:last-child').innerHTML;
+                if (hasButton != "Undercut") {
+                    const rowID = row.getAttribute('data-id');
+                    const currencyRatio = Number(row.querySelector(".per-have > span:last-child").innerText.match(/[\d\.]*/));
+                    const currencyType = row.querySelector('.price-right .currency-text').innerText;
+                    const sellType = row.querySelector('.price-left .currency-text').innerText;
+                    
+                    if (currencyType == 'Chaos Orb'){
+                        let button = document.createElement("button");
+                        button.innerHTML = "Undercut";
+                        button.classList.add("btn", "btn-default");
+                        button.name = `${rowID}_Undercut`
+                        button.onclick = function () {
+                            clipboardMod(currencyRatio, currencyType, sellType);
+                        };
+                        row.querySelector('.btns > .btn-group').append(button);
+                    } 
+                }
+            }
+        }
+    }
+}
+
+async function clipboardMod(currencyRatio, currencyType, sellType) {
+    if (currencyType != 'Chaos Orb') {
+        return;
+    }
+
+    let itemText = await navigator.clipboard.readText();
+    let clipboardContainsSellType = itemText.indexOf(sellType);
+    if (clipboardContainsSellType == -1) {
+        alert("Clipboard does not contain the description of the item in this result.");
+        return
+    }
+
+    let stackSize = itemText.match(/Stack Size: ([\d\.,]+)/);
+    if (stackSize) {
+        stackSize = Number(stackSize[1].replaceAll(",", ""));
+        let price = currencyRatio * stackSize;
+        if (price - Math.floor(price) === 0) {
+            price -= 1;
+        } else {
+            price = Math.floor(price);
+        }
+
+        navigator.clipboard.writeText(`~price ${price}/${stackSize} ${currencyType}`)
+    } else {
+        alert("Error getting stack size");
+    }
+    
+}
+
+
+windowObserver.observe(target, {childList: true, subtree: true});

7a3cac9444cfaf489978fcbbf40e3e3d36ba467a
empratts <65209619+empratts@users.noreply.github.com>
2024-01-05 06:54:21 -0600

    Options (#6)
    * Added basic options
    
    * Text box in options and updated Readme
    
    * currency view layout
    
    ---------
    
    Co-authored-by: Eric Pratts <eric.pratts.1@us.af.mil>

---
+++ options/options.js
@@ -0,0 +1,31 @@
+// Saves options to chrome.storage
+const saveOptions = () => {
+    const search_percent = Math.max(Math.min(Math.floor(document.getElementById('search_percent').value), 5), 100);
+  
+    chrome.storage.sync.set(
+      { searchPercent: search_percent },
+      () => {
+        // Update status to let user know options were saved.
+        const status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(() => {
+          status.textContent = '';
+        }, 750);
+      }
+    );
+  };
+  
+  // Restores select box and checkbox state using the preferences
+  // stored in chrome.storage.
+  const restoreOptions = () => {
+    chrome.storage.sync.get(
+      { searchPercent: 100 },
+      (items) => {
+        document.getElementById('search_percent').value = items.searchPercent;
+        //document.getElementById('like').checked = items.likesColor;
+      }
+    );
+  };
+  
+  document.addEventListener('DOMContentLoaded', restoreOptions);
+  document.getElementById('save').addEventListener('click', saveOptions);

66926964e6f196531c294ef98239141984a4483a
Eric Pratts <pratts.eric@gmail.com>
2024-02-20 04:36:54 -0600

    Options update

--- options/options.js
+++ options/options.js
@@ -1,8 +1,9 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
     const search_percent = Math.max(Math.min(Math.floor(document.getElementById('search_percent').value), 100), 5);
+    const advanced_pricing = document.getElementById("adv_pricing").checked;
   
-    chrome.storage.sync.set({ searchPercent: search_percent }).then(() => {
+    chrome.storage.sync.set({ searchPercent: search_percent , advancedPricing: advanced_pricing}).then(() => {
         // Update status to let user know options were saved.
         const status = document.getElementById('status');
         status.textContent = 'Options saved.';
@@ -13,8 +14,9 @@ const saveOptions = () => {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 const restoreOptions = () => {
-    chrome.storage.sync.get(["searchPercent"]).then((items) => {
+    chrome.storage.sync.get(["searchPercent", "advancedPricing"]).then((items) => {
         document.getElementById('search_percent').setAttribute("value", items.searchPercent);
+        document.getElementById("adv_pricing").checked = items.advancedPricing;
     });
 };
   

befe1a44df702dd91ef98627d3d4d6546698ae99
Eric Pratts <pratts.eric@gmail.com>
2024-02-20 05:27:06 -0600

    Adv Search button opens window

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,6 @@
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(sender.tab ?
-                    "from a content script:" + sender.tab.url :
-                    "from the extension");
-        if (request.greeting === "hello") {
-            console.log("Got Hello.");
-            chrome.tabs.create({url: "advanced/pricing.html?x=44"})
-        }
+        chrome.tabs.create({url: `advanced/pricing.html?${request.price_info}`})
     }
 );
